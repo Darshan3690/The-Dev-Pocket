81bde275f95bc02f34d1b9922ce32f5718c6fb5a
Abhin Vaidya <abhinavvaidya2005@gmail.com>
2026-01-06 13:11:31 +0530

    feat(notes): implement localStorage persistence and fix build errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
- feat/search-blog-filter
-import { calculateReadingTime } from '../../js/utils/readingTime';
-
 import { calculateReadingTime } from '@/utils/readingTime';
 
 import "./globals.css";
