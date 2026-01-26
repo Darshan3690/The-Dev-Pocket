71810fa7c27ebbc7d1afd4b6f835d32606acaef1
Ayaanshaikh12243 <Ayaansalman74@gmail.com>
2026-01-27 02:21:04 +0530

    Career Guidance:

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -20,6 +20,7 @@ export default function Navbar() {
     { name: "Features", link: "#features" },
     { name: "Resources", link: "/resources" },
     { name: "Practice Hub", link: "/practice-hub" },
+    { name: "Career Guidance", link: "/career-guidance" },
     { name: "Pricing", link: "#pricing" },
     { name: "About", link: "/about" },
   ];
